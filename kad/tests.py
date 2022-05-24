456c501c31ae9b4a09dbd9c2bcdf95d539120bcd
farkon00 <59134591+farkon00@users.noreply.github.com>
2022-05-24 23:28:18 +0300

    Removed brackets for asserts

--- kad/tests.py
+++ kad/tests.py
@@ -5,6 +5,6 @@ import kad
 dictionary = {'a': 5, 'b': True}
 
 kad_object = kad.to_kad(dictionary)
-assert(kad_object.b)
-assert(kad_object.a == 5)
+assert kad_object.b
+assert kad_object.a == 5
 print("All tests pass.")
